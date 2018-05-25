4469c3f65e85fb43508df85edcaf5e3bab9beabf
Mac Read <james.macgregor.read@gmail.com>
2018-05-25 12:17:27 -0600

    1:2

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -2,11 +2,52 @@ import React, { Component } from 'react';
 import Product from '../../component/product/Product'
 
 export default class Dashboard extends Component {
+    constructor() {
+    super()
+        
+        this.state = {
+            products: [
+                {id: 0,
+                    url: 'https://images.dog.ceo/breeds/dane-great/n02109047_23852.jpg',
+                    name: 'Dane',
+                    price: 1000
+                },
+                {id: 1,
+                    url: 'https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_3074.jpg',
+                    name: 'Chip',
+                    price: 1002
+                },
+                {id: 2,
+                    url: 'https://images.dog.ceo/breeds/appenzeller/n02107908_795.jpg',
+                    name: 'Frosty',
+                    price: 1003
+                },
+                {id: 3,
+                    url: 'https://images.dog.ceo/breeds/bulldog-boston/n02096585_1583.jpg',
+                    name: 'Fisher',
+                    price: 1004
+                }
+            ]
+        }
+    }
+
+
+
     render() {
         return (
             <div>
                 Dashboard
-                <Product />
+                {
+                    this.state.products.map( product => (
+                        <Product 
+                            id={product.id} 
+                            key={product.id} 
+                            name={product.name} 
+                            imageURL={product.url} 
+                            price={product.price}
+                        />
+                    ))
+                }
             </div> 
         )
     }
--- src/component/product/Product.js
+++ src/component/product/Product.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function Product() {
+export default function Product(props) {
   return (
-    <div>
-        Product
+    <div key={props.key}>
+        <img src={ props.imageURL } alt="product" className=""/>
+        <span className="">Name: {props.name}</span>
+        <span className="">Price: {props.price}</span>
     </div> 
   )
 }
