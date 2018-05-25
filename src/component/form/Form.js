afc7b4d19baaa0c1c1c81b48fdccb7c9d69880b2
Mac Read <james.macgregor.read@gmail.com>
2018-05-25 11:57:55 -0600

    1:1

--- src/component/form/Form.js
+++ src/component/form/Form.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 
 export default class Form extends Component {
+    constructor() {
+    super()
+        
+        this.state = {
+                productName: "",
+                price: "",
+                imageURL: ""
+            }
+    }
+
+    updateProductName(val){
+        this.setState({ productName: val })
+    }
+
+    updatePrice(val){
+        this.setState({ price: val })
+    }
+
+    updateImageURL(val){
+        this.setState({ imageURL: val })
+    }
+
+    addProduct(){
+
+    }
+
+    userCancel(){
+        this.setState({
+            productName: "",
+            price: "",
+            imageURL: ""
+        })
+    }
+
     render() {
         return (
-            <div>Form</div> 
+            <div>
+                Product Name: <input type='' className='' onChange={ ( e ) => this.updateProductName( e.target.value ) }/>
+                Price: <input type='' className=''onChange={ ( e ) => this.updatePrice( e.target.value ) }/>
+                Image URL: <input type='' className=''onChange={ ( e ) => this.updateImageURL( e.target.value ) }/>
+                <button type='' className='' onClick={()=> this.addProduct()}>Add</button>
+                <button type='' className='' onClick={()=> this.userCancel()}>Cancel</button>
+            </div> 
         )
     }
 }
