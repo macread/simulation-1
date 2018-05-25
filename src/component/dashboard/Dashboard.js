8682f3f20696c33236e9aeaaa882b27e40ba5cd9
Mac Read <james.macgregor.read@gmail.com>
2018-05-25 12:43:25 -0600

    1:4

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -1,37 +1,24 @@
 import React, { Component } from 'react';
 import Product from '../../component/product/Product'
+import axios from 'axios';
 
 export default class Dashboard extends Component {
     constructor() {
     super()
         
         this.state = {
-            products: [
-                {id: 0,
-                    url: 'https://images.dog.ceo/breeds/dane-great/n02109047_23852.jpg',
-                    name: 'Dane',
-                    price: 1000
-                },
-                {id: 1,
-                    url: 'https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_3074.jpg',
-                    name: 'Chip',
-                    price: 1002
-                },
-                {id: 2,
-                    url: 'https://images.dog.ceo/breeds/appenzeller/n02107908_795.jpg',
-                    name: 'Frosty',
-                    price: 1003
-                },
-                {id: 3,
-                    url: 'https://images.dog.ceo/breeds/bulldog-boston/n02096585_1583.jpg',
-                    name: 'Fisher',
-                    price: 1004
-                }
-            ]
+            products: []
         }
     }
 
-
+    componentDidMount() {
+        axios.get('api/inventory').then( results => {
+            this.setState({ products: results.data });
+        });
+       this.setState({
+           randomDogURL: this.state.defaultDog
+       })
+    }
 
     render() {
         return (
@@ -43,7 +30,7 @@ export default class Dashboard extends Component {
                             id={product.id} 
                             key={product.id} 
                             name={product.name} 
-                            imageURL={product.url} 
+                            imageURL={product.image_url} 
                             price={product.price}
                         />
                     ))
